8f3913597b8c9100437e733295a6adc01185d479
jefQuery <jeffrey.milberger@gmail.com>
2017-03-30 14:42:07 -0700

    Add functional Event Owner Reminder button, dynamic message name and time

--- server/routes/send_sms.js
+++ server/routes/send_sms.js
@@ -1,7 +1,12 @@
 let twilioClient = require('../twilioClient.js');
 
-module.exports = function (req, res) {
-  twilioClient.sendSms(function(err, data) {
+module.exports = (req, res) => {
+  let sringEvent = req.body['event'];
+  let event = JSON.parse(sringEvent);
+  
+  //add dbquery here
+
+  twilioClient.sendSms(event, function(err, data) {
     if (err) {
       res.status(404).send('Could not send SMS');
     } else {
