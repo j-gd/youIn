584a17d5651ed10c9db2be9ed94fff57b1f9cd61
burt23 <mail4mdb@gmail.com>
2017-03-31 15:03:18 -0700

    Created initial grid for full page layout of 3 left and 9 right using bootstrap

--- src/client/app/Event.jsx
+++ src/client/app/Event.jsx
@@ -6,20 +6,23 @@ class Event extends React.Component {
     super(props);
   }
 
+    // <div className="event-title"> Cookout ****** </div>
+    // <div className="row">
+    //     <div className="col-md-8"> Details</div>
+    //     <div className="col-md-4 event-chat"> Chat </div>
+    // </div>
   render () {
-	  return (
+    return (
 
-<div className="container-fluid">
-  <div className="col-md-3 directory"> col-md-4
+<div className="container-fluid main">
+
+  <div className="col-md-3 directory"> col-md-3
   </div>
+
   <div className="col-md-9">
-    <div className="event-title"> Cookout ****** </div>
-    <div className="row">
-        <div className="col-md-8"> Details</div>
-        <div className="col-md-4 event-chat"> Chat </div>
-    </div>
   </div>
-</div>	  	
+
+</div>
 	  )
   }
 }
@@ -29,4 +32,4 @@ export default Event;
 
     // </div>
       // <div className="row-md-6"> # cookout </div>
-      // <div className="row-md-6"> row-md-6 
+      // <div className="row-md-6"> row-md-6
