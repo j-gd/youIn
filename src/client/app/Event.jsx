9fd6fbffa93e61af87e306565a1b9a799031e672
J-G Demathieu <jdemathieu@gmail.com>
2017-03-31 11:57:59 -0700

    Add initial CSS for site redesign, create bootstrap grid

--- src/client/app/Event.jsx
+++ src/client/app/Event.jsx
@@ -9,11 +9,24 @@ class Event extends React.Component {
   render () {
 	  return (
 
-<div class="container-fluid">
-   <h1>This is slack-like</h1>
+<div className="container-fluid">
+  <div className="col-md-3 directory"> col-md-4
+  </div>
+  <div className="col-md-9">
+    <div className="event-title"> Cookout ****** </div>
+    <div className="row">
+        <div className="col-md-8"> Details</div>
+        <div className="col-md-4 event-chat"> Chat </div>
+    </div>
+  </div>
 </div>	  	
 	  )
   }
 }
 
 export default Event;
+
+
+    // </div>
+      // <div className="row-md-6"> # cookout </div>
+      // <div className="row-md-6"> row-md-6 
