a407fc2eb6e8a3a2704a591f5d860566549d0c03
burt23 <mail4mdb@gmail.com>
2017-04-01 11:37:13 -0700

    Linked New version of homepage with existing, now linking all components

--- src/client/app/Account.jsx
+++ src/client/app/Account.jsx
@@ -14,20 +14,20 @@ class Account extends React.Component {
     <div>
       <div className='col-sm-12 accountTopBar'>
         <div className='row-sm-3 accountPhoto'>
-          {/* NEEDS LINKING TO RENDER <img src={this.props.friends[0].photourl} />*/}
+         <img src={this.props.friends[0].photourl} />
         </div>
         <div className='row-sm-9 accountName'>
-          <h2>{this.props.userName}<span className='caret-down'>&#9660;</span></h2>
+          <h2>{this.props.username}<span className='caret-down'>&#9660;</span></h2>
         </div>
       </div>
       <div className='col-sm-12 accountMyEvents'>
         <h2>My Events</h2>
-        <MyEvents myEvents={this.props.ownerEvents}/>
+        <MyEvents events={this.props.myEvents}/>
 
       </div>
       <div className='col-sm-12 accountFriendEvents'>
         <h2>Friend's Events</h2>
-        <MyEvents friendEvents={this.props.friendEvents}/>
+        <MyEvents events={this.props.friendEvents}/>
       </div>
     </div>
     )
--- src/client/app/Homepage.jsx
+++ src/client/app/Homepage.jsx
@@ -5,12 +5,20 @@ import CreateEventButton from './CreateEventButton.jsx';
 import LogoutButton from './LogoutButton.jsx';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
+import Chat from './ChatCopy.jsx';
+import TopBar from './TopBar.jsx';
+import CreateEvent from './CreateEvent.jsx';
+import EventOverview from './EventOverview.jsx';
+import Account from './Account.jsx';
+// import {render} from 'react-dom';
+
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       intervalId: null,
-      clickedEvent: false
+      clickedEvent: false,
+      createEvent: false,
     };
   }
 
@@ -44,41 +52,70 @@ class Homepage extends React.Component {
   }
 
   render() {
-
     return (
-      <div>
-        <div className="container">
-          <div className="page-header">
-           <h2 id='userName'>Welcome <span id="headerName">{this.props.userName}</span></h2>
-            <LogoutButton />
-          </div>
-          <CreateEventButton
-          history={this.props.history}
-          friends={this.props.friends}
-          getEvents={this.props.getEvents}/>
-          <br /><br />
-          <div className='container events'>
-            <br></br><br></br>
-            <h2 id="my-events-title" className='header-inner'> My Events</h2>
-            <OwnerEventList
-            ownerEventsArr={this.props.ownerEvents}
-            accessToken={this.props.accessToken}
-            getEvents={this.props.getEvents}
-            history={this.props.history}/>
-          </div>
-            <br /><br />
-          <div className='container events'>
-            <h2 id="friend-events-title"className='header-inner'> Friend Events</h2>
-            <FriendEventList accessToken={this.props.accessToken}
-            friendEventsArr={this.props.friendEvents}
-            getEvents={this.props.getEvents}/>
-          </div>
-        </div>
-        <Link to='/slack' > Link to new design </Link>
+    // { -------PAGE WRAPPER------ }
+    <div className="container-fluid main">
+
+      {/* -------LEFT HAND COLUMN------- */}
+      <div className="col-sm-3 account">
+        <Account username={this.props.userName} friends = {this.props.friends} myEvents={this.props.ownerEvents} friendEvents={this.props.friendEvents} />
       </div>
+
+      {/* -------RIGHT HAND COLUMN------- */}
+      <div className="col-sm-9 right9">
+
+         {/* -------TOPBAR------- */}
+        <div className='col-sm-12 topbar'>
+          <TopBar owner={this.props.friends[0]}/>
+        </div>
+
+         {/* -------MAIN------- */}
+        <div className='col-sm-8 mainview'>
+          {this.state.clickedEvent ? <CreateEvent/> : <EventOverview/>}
+        </div>
+
+         {/* -------CHAT------- */}
+        <div className='col-sm-4 right4'>
+          <Chat/>
+        </div>
+
+      </div>
+    </div>
     )
   }
 }
 
 
 export default Homepage;
+    // return (
+    //   <div>
+    //     <div className="container">
+    //       <div className="page-header">
+    //        <h2 id='userName'>Welcome <span id="headerName">{this.props.userName}</span></h2>
+    //         <LogoutButton />
+    //       </div>
+    //       <CreateEventButton
+    //       history={this.props.history}
+    //       friends={this.props.friends}
+    //       getEvents={this.props.getEvents}/>
+    //       <br /><br />
+    //       <div className='container events'>
+    //         <br></br><br></br>
+    //         <h2 id="my-events-title" className='header-inner'> My Events</h2>
+    //         <OwnerEventList
+    //         ownerEventsArr={this.props.ownerEvents}
+    //         accessToken={this.props.accessToken}
+    //         getEvents={this.props.getEvents}
+    //         history={this.props.history}/>
+    //       </div>
+    //         <br /><br />
+    //       <div className='container events'>
+    //         <h2 id="friend-events-title"className='header-inner'> Friend Events</h2>
+    //         <FriendEventList accessToken={this.props.accessToken}
+    //         friendEventsArr={this.props.friendEvents}
+    //         getEvents={this.props.getEvents}/>
+    //       </div>
+    //     </div>
+    //     <Link to='/slack' > Link to new design </Link>
+    //   </div>
+    // )
